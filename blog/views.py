24f27d887b5ae114da16a2ee6bbc3dbfe362cf69
nalouddin <nalouddin@gmail.com>
2023-03-17 15:41:21 +0500

    blog ga ditail qo'shildi

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Blog
 
 # Create your views here.
 def home(request):
-    blogs = Blog.objects.all()
+    blogs = Blog.objects.order_by('-date')
     return render(request, 'blog/blog.html', {'blogs': blogs})
+
+
+def detail(request, id):
+    blog = get_object_or_404(Blog, pk=id)
+    return render(request, 'blog/detail.html', {'blog': blog})
